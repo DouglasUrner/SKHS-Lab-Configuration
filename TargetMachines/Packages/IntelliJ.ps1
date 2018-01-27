148c4a7fe1d6e5faeba8059087238ca0df7f80a1
Douglas Urner <dlu@canishe.com>
2018-01-27 14:37:42 -0800

    Pull in old package scripts.

---
+++ TargetMachines/Packages/IntelliJ.ps1
@@ -0,0 +1,21 @@
+﻿Configuration IntelliJ
+{
+    param ($NodeName)
+
+    Import-DscResource -Module cChoco
+
+    Node $NodeName
+    {
+        cChocoInstaller installChoco {
+            InstallDir = "C:\choco"
+        }
+
+        cChocoPackageInstaller intellij {
+            Name = "intellijidea-ultimate"
+            #AutoUpgrade = $True
+            DependsOn = "[cChocoInstaller]installChoco"
+        }
+    }
+ }
+
+ Start-DscConfiguration -Path U:\src\SKHS-Lab-Configuration\StudentMachine -ComputerName "HS022371.skitsap.wednet.edu"
