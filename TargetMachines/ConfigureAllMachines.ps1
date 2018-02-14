c2c527cbbbdc03eac863f2bb4c2f5d141249342e
Douglas Urner <dlu@canishe.com>
2018-02-14 15:34:55 -0800

    Fix path.

--- TargetMachines/ConfigureAllMachines.ps1
+++ TargetMachines/ConfigureAllMachines.ps1
@@ -1,12 +1,12 @@
 ﻿# Session setup
-$Hostnames = cat U:\src\SKHS-Lab-Configuration\TargetMachines\hostnames.txt | foreach { $_ + '.skitsap.wednet.edu' }
-$Hostnames = 'HS022370.skitsap.wednet.edu'
+$Hostnames = cat U:\Source\SKHS-Lab-Configuration\TargetMachines\hostnames.txt | foreach { $_ + '.skitsap.wednet.edu' }
+$Hostnames = 'HS022382.skitsap.wednet.edu'
 $Sessions = New-PSSession $Hostnames
 
 # Copy packages.config
 invoke-command -Session $Sessions -ScriptBlock { mkdir C:\"Lab Files" }
 echo "Created Lab Files folder."
-cp U:\src\SKHS-Lab-Configuration\TargetMachines\packages.config C:\"Lab Files" -ToSession $Sessions
+cp U:\Source\SKHS-Lab-Configuration\TargetMachines\packages.config C:\"Lab Files" -ToSession $Sessions
 echo "Copied packages.config."
 
 # Run the script
