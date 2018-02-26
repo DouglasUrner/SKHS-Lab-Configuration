5be13d022d7190648f7e98f7e9246cf85da6feb5
Douglas Urner <dlu@canishe.com>
2018-02-26 06:50:32 -0800

    Make work after imaging lab.

--- TargetMachines/ConfigureAllMachines.ps1
+++ TargetMachines/ConfigureAllMachines.ps1
@@ -1,13 +1,20 @@
 ﻿# Session setup
 $Hostnames = cat U:\Source\SKHS-Lab-Configuration\TargetMachines\hostnames.txt | foreach { $_ + '.skitsap.wednet.edu' }
-$Hostnames = 'HS022382.skitsap.wednet.edu'
 $Sessions = New-PSSession $Hostnames
 
 # Copy packages.config
 invoke-command -Session $Sessions -ScriptBlock { mkdir C:\"Lab Files" }
-echo "Created Lab Files folder."
-cp U:\Source\SKHS-Lab-Configuration\TargetMachines\packages.config C:\"Lab Files" -ToSession $Sessions
-echo "Copied packages.config."
+    echo "Created Lab Files folder."
+    foreach ($session in $sessions) {
+    cp U:\Source\SKHS-Lab-Configuration\TargetMachines\packages.config C:\"Lab Files" -ToSession $Session
+    echo "Copied packages.config"
+    if(-not (invoke-command -session $session -scriptblock { Test-Path "C:\Lab Files\MIT_App_Inventor_Tools_2.3.0_win_setup.exe" })) {
+        cp U:\Source\SKHS-Lab-Configuration\TargetMachines\MIT_App_Inventor_Tools_2.3.0_win_setup.exe C:\"Lab Files" -ToSession $Session
+        echo "Copied AI Tools"
+    } else {
+        echo "AI tools already exist."
+    }
+}
 
 # Run the script
 invoke-command -Session $Sessions -FilePath U:\Source\SKHS-Lab-Configuration\TargetMachines\TargetScript.ps1 | foreach { echo ($_.PSComputerName+': '+$_) }
--- TargetMachines/TargetScript.ps1
+++ TargetMachines/TargetScript.ps1
@@ -8,8 +8,17 @@
     echo "already has choco"
 }
 
+& "C:\Lab Files\MIT_App_Inventor_Tools_2.3.0_win_setup.exe" /S
+echo "Started AppInventor Installer"
+
 echo "starting to install packages"
-choco install "C:\Lab Files\packages.config" -y
+choco install "C:\Lab Files\packages.config" -y --no-progress
 echo "finished installing packages"
+
+$WshShell = New-Object -ComObject WScript.Shell
+$AIShortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\MIT App Inventor Tools\aiStarter.lnk")
+$AIShortcut.TargetPath = 'C:\Program Files (x86)\AppInventor\aiStarter.exe'
+$AIShortcut.Save()
+echo "created AppInventor shortcut"
 
 echo "done"
