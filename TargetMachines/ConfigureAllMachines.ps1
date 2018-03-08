f64004d625928b0f32a8547bb93d2c43055fb831
Douglas Urner <dlu@canishe.com>
2018-03-07 16:48:30 -0800

    Destop shortcuts and .NET SDK.

--- TargetMachines/ConfigureAllMachines.ps1
+++ TargetMachines/ConfigureAllMachines.ps1
@@ -1,15 +1,16 @@
-﻿# Session setup
-$Hostnames = cat U:\Source\SKHS-Lab-Configuration\TargetMachines\hostnames.txt | foreach { $_ + '.skitsap.wednet.edu' }
+﻿$LabConfigDir = "U:\Source\Admin\SKHS-Lab-Configuration"
+# Session setup
+$Hostnames = cat $LabConfigDir\TargetMachines\hostnames.txt | foreach { $_ + '.skitsap.wednet.edu' }
 $Sessions = New-PSSession $Hostnames
 
 # Copy packages.config
 invoke-command -Session $Sessions -ScriptBlock { mkdir C:\"Lab Files" }
     echo "Created Lab Files folder."
     foreach ($session in $sessions) {
-    cp U:\Source\SKHS-Lab-Configuration\TargetMachines\packages.config C:\"Lab Files" -ToSession $Session
+    cp $LabConfigDir\TargetMachines\packages.config C:\"Lab Files" -ToSession $Session
     echo "Copied packages.config"
     if(-not (invoke-command -session $session -scriptblock { Test-Path "C:\Lab Files\MIT_App_Inventor_Tools_2.3.0_win_setup.exe" })) {
-        cp U:\Source\SKHS-Lab-Configuration\TargetMachines\MIT_App_Inventor_Tools_2.3.0_win_setup.exe C:\"Lab Files" -ToSession $Session
+        cp $LabConfigDir\TargetMachines\MIT_App_Inventor_Tools_2.3.0_win_setup.exe C:\"Lab Files" -ToSession $Session
         echo "Copied AI Tools"
     } else {
         echo "AI tools already exist."
@@ -17,4 +18,4 @@ invoke-command -Session $Sessions -ScriptBlock { mkdir C:\"Lab Files" }
 }
 
 # Run the script
-invoke-command -Session $Sessions -FilePath U:\Source\SKHS-Lab-Configuration\TargetMachines\TargetScript.ps1 | foreach { echo ($_.PSComputerName+': '+$_) }
+invoke-command -Session $Sessions -FilePath $LabConfigDir\TargetMachines\TargetScript.ps1 | foreach { echo ($_.PSComputerName+': '+$_) }
--- TargetMachines/TargetScript.ps1
+++ TargetMachines/TargetScript.ps1
@@ -15,10 +15,29 @@ echo "starting to install packages"
 choco install "C:\Lab Files\packages.config" -y --no-progress
 echo "finished installing packages"
 
+echo "Creating shortcuts... "
+
 $WshShell = New-Object -ComObject WScript.Shell
 $AIShortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\MIT App Inventor Tools\aiStarter.lnk")
 $AIShortcut.TargetPath = 'C:\Program Files (x86)\AppInventor\aiStarter.exe'
 $AIShortcut.Save()
-echo "created AppInventor shortcut"
+echo "AppInventor "
+
+# $WshShell = New-Object -ComObject WScript.Shell
+# $AIShortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\JetBrains\IntelliJ.lnk")
+# $AIShortcut.TargetPath = 'C:\Program Files (x86)\JetBrains\IntelliJ IDEA 2017.3.2\bin\idea64.exe'
+# $AIShortcut.Save()
+
+$WshShell = New-Object -ComObject WScript.Shell
+$AIShortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\IntelliJ.lnk")
+$AIShortcut.TargetPath = 'C:\Program Files (x86)\JetBrains\IntelliJ IDEA 2017.3.2\bin\idea64.exe'
+$AIShortcut.Save()
+echo "IntelliJ "
+
+$WshShell = New-Object -ComObject WScript.Shell
+$AIShortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\Rider.lnk")
+$AIShortcut.TargetPath = 'C:\Program Files (x86)\JetBrains\JetBrains Rider 2017.2.1\bin\rider64.exe'
+$AIShortcut.Save()
+echo "Rider "
 
 echo "done"
